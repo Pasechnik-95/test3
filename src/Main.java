2af1238dd2eb1d7e5cdff9d23c773251c8e5c957
Admiral <pasechnik-95@yandex.ru>
2023-09-22 22:13:35 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String trafficLightColor = "green"; //Переменная для хранения сигнала светофора
+        switch (trafficLightColor) {
+            case "red":
+                System.out.println("Остановится!");
+                break;
+            case "yellow":
+                System.out.println("Подождать.");
+                break;
+            case "green":
+                System.out.println("Ехать.");
+                break;
+        }
+    }
+}
